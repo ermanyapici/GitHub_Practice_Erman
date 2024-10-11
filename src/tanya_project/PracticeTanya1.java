18c817f8e75a17063786eb51ee2f78d9b688b7da
ermanyapici <erman.yapici@yahoo.com>
2024-10-10 19:39:15 -0500

    Update Tanya's file

--- src/tanya_project/PracticeTanya1.java
+++ src/tanya_project/PracticeTanya1.java
@@ -3,4 +3,6 @@ package tanya_project;
 public class PracticeTanya1 {
 
     //Tanya created new project and new class
+
+    // Tanya has merged the file already and now it is time to update this and push it.
 }
